d18f8576112a071808aef6d5af633f39517ea1fc
Bard <bard@example.com>
2025-10-26 13:56:23 +0800

    cleanup code

--- src/main/java/bard/model/CurrencyResponce.java
+++ src/main/java/bard/model/CurrencyResponce.java
@@ -1,10 +1,10 @@
 package bard.model;
 
 public class CurrencyResponce {
-    private Long id;
     private final String code;
     private final String fullName;
     private final String sign;
+    private Long id;
 
     public CurrencyResponce(String code, String fullName, String sign) {
         this.code = code;
